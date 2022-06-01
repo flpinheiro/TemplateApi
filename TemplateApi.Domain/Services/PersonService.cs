26a982467aedc37e2fd809cc6edd587feae947f0
Felipe Pinheiro <flpinheiro@gmail.com>
2022-06-01 14:32:40 -0300

    update person error

--- TemplateApi.Domain/Services/PersonService.cs
+++ TemplateApi.Domain/Services/PersonService.cs
@@ -21,7 +21,7 @@ internal class PersonService : IPersonService
             var model = _uow.Mapper.Map<Person>(person);
             person.Id = _uow.PersonRepository.Add(model);
             await _uow.SaveAsync();
-            if (person.Name == "string") throw new Exception("String name exception");
+            if (person.Name == "string") throw new PersonNotFoundException();
             return person;
         }
         catch (Exception ex)
